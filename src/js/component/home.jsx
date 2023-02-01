9134ac60454d8fd4c97e9387a89eff881b113fae
paul-war <pablodanielguerra@gmail.com>
2023-02-01 19:17:45 +0000

    version4

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,9 +10,9 @@ const [clicked, setClicked] = useState("");
 return (
 	<div className="text-center traffic-light">
 		<div className="stick"></div>
-		<div onClick={() => setClicked("red")} className={`redLight ${clicked === "red" ? "clicked" : ""}`}></div>
-		<div onClick={() => setClicked("yellow")} className={`yellowLight ${clicked === "yellow" ? "clicked" : ""}`}></div>
-		<div onClick={() => setClicked("green")} className={`greenLight ${clicked === "green" ? "clicked" : ""}`}></div>
+		<div onClick={() => setClicked("red")} className={`redLight ${clicked === "red" ? " clicked" : ""}`}></div>
+		<div onClick={() => setClicked("yellow")} className={`yellowLight ${clicked === "yellow" ? " clicked" : ""}`}></div>
+		<div onClick={() => setClicked("green")} className={`greenLight ${clicked === "green" ? " clicked" : ""}`}></div>
 	</div>
 	);
 } 
